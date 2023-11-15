ea9fcc108bc8490a31f74bd423b2a52b8ae3a0f3
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-15 20:03:00 +0530

    feat: server setup

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import { config } from "dotenv";
+import express, { Request, Response } from "express";
+
+config()
+const app = express();
+
+
+// Test
+app.get("/", async (req: Request, res: Response) => {
+    res.status(200).send("<h2>Server Working!👍👍👍</h2>")
+})
+
+const PORT = process.env.PORT || 4000;
+const start = () => {
+	try {
+		app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+start();
