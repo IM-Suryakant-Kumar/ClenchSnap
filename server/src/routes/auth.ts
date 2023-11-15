b982ca4a71e297bb68796ac0f9bfeb70cf5dd243
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-15 23:39:44 +0530

    fix: logout

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createUser, login, logout } from "../controllers/auth";
+import { authenticateUser } from "../middleware/authentication";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(createUser)
-router.route("/login").post(login)
-router.route("/logout").get(logout)
+router.route("/register").post(createUser);
+router.route("/login").post(login);
+router.route("/logout").get(authenticateUser, logout);
 
-export default router
+export default router;
