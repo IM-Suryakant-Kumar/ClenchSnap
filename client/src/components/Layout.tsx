88debf1ae00fdf9b62d8a24287896dff3f9e6677
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-19 10:14:13 +0530

    feat: added react-toastify

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import { Slide, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
 	return (
@@ -8,6 +10,11 @@ const Layout = () => {
 			<div className="mt-[6em] sm:mt-[4em]">
 				<Outlet />
 			</div>
+			<ToastContainer
+				autoClose={1000}
+				transition={Slide}
+				theme="colored"
+			/>
 		</div>
 	);
 };
