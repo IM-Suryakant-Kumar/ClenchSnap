bb5fad553ff2aa41af0ef53c1aed5f1c21085d44
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-19 19:27:35 +0530

    feat: added authApi

--- client/src/utils/authApi.ts
+++ client/src/utils/authApi.ts
@@ -1,48 +1,66 @@
 import { IError } from "../types/response";
 import { ILogCred, IRegCred } from "../types/user";
 import axios from "./axios";
+import {
+	AddTokenToLocalStorage,
+	getTokenFromLocalStorage,
+} from "./handleToken";
 
 // login
 export const login = async (logCred: ILogCred) => {
-    try {
-        const { data } = await axios.post("/login", logCred)
-        return data
-    } catch (error) {
-        const { response: { data } } = error as IError
-        console.log(data)
-        return data
-    }
-}
+	try {
+		const { data } = await axios.post("/login", logCred);
+		AddTokenToLocalStorage(data.token);
+		return data;
+	} catch (error) {
+		const {
+			response: { data },
+		} = error as IError;
+		console.log(data);
+		return data;
+	}
+};
 // guest login
 export const guestLogin = async () => {
-    try {
-        const { data } = await axios.get("/guest-login")
-        return data
-    } catch (error) {
-        const { response: { data } } = error as IError
-        console.log(data)
-        return data
-    }
-}
+	try {
+		const { data } = await axios.get("/guest-login");
+		AddTokenToLocalStorage(data.token);
+		return data;
+	} catch (error) {
+		const {
+			response: { data },
+		} = error as IError;
+		console.log(data);
+		return data;
+	}
+};
 // signup
 export const signup = async (regCred: IRegCred) => {
-    try {
-        const { data } = await axios.post("/register", regCred)
-        return data
-    } catch (error) {
-        const { response: { data } } = error as IError
-        console.log(data)
-        return data
-    }
-}
+	try {
+		const { data } = await axios.post("/register", regCred);
+		AddTokenToLocalStorage(data.token);
+		return data;
+	} catch (error) {
+		const {
+			response: { data },
+		} = error as IError;
+		console.log(data);
+		return data;
+	}
+};
 // logout
 export const logout = async () => {
-    try {
-        const { data } = await axios.get("/logout")
-        return data
-    } catch (error) {
-        const { response: { data } } = error as IError
-        console.log(data)
-        return data
-    }
-}
+	try {
+		const token = getTokenFromLocalStorage();
+		const { data } = await axios.get("/logout", {
+			headers: { Authorization: `Bearer ${token}` },
+		});
+		return data;
+	} catch (error) {
+		const {
+			response: { data },
+		} = error as IError;
+		console.log(data);
+		return data;
+	}
+};
