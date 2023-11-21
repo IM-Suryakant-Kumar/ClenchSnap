97c3a4988488c3a3021a5ef0fe07991e34a5af98
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-21 22:45:25 +0530

    feat: Added profile pic component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,9 +2,14 @@ import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/share.svg";
 import { MdSearch } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import { useUser } from "../contexts";
+import ProfilePic from "./ProfilePic";
 
 const Navbar = () => {
 	const navigate = useNavigate();
+	const {
+		userState: { user },
+	} = useUser();
 
 	return (
 		<header className="h-[6rem] w-full sm:h-[4rem] bg-secondary-cl px-1 fixed top-0">
@@ -25,7 +30,17 @@ const Navbar = () => {
 					className="w-[25%] sm:w-[10%] sm:order-3 flex items-center justify-end pr-[1em] text-[1.5rem] sm:text-[1.8rem] cursor-pointer"
 					onClick={() => navigate("/settings")}
 				>
-					<CgProfile color="#3a86ff" />
+					{user ? (
+						<ProfilePic
+							name={user.name}
+							avatar={user.avatar}
+							width="1.5rem"
+							height="1.5rem"
+							size="1.2rem"
+						/>
+					) : (
+						<CgProfile color="#3a86ff" />
+					)}
 				</div>
 				<div className="w-[100%] sm:w-[56%] flex items-center justify-center sm:justify-end sm:order-2">
 					<form className="w-[90%] sm:max-w-[30rem] h-[1.6rem] sm:h-[1.8rem] bg-primary-cl rounded-md flex">
---
+++ client/src/components/ProfilePic.tsx
@@ -0,0 +1,41 @@
+type Props = {
+	name: string;
+	avatar: string;
+	width: string;
+	height: string;
+	size: string;
+};
+
+const ProfilePic: React.FC<Props> = ({ name, avatar, width, height, size }) => {
+	const colors = ["ffb703", "fb8500", "8338ec", "41ead4", "ff006e", "fb5607"];
+	const randomIndex = Math.floor(Math.random() * colors.length);
+	const color = `#${colors[randomIndex]}`;
+
+	return (
+		<div
+			className={`bg-logo-cl rounded-full p-[0.1em]`}
+			style={{ width, height }}
+		>
+			{avatar ? (
+				<div
+					className="text-primary-cl w-full h-full rounded-full flex justify-center items-center"
+					style={{ backgroundColor: color, fontSize: size }}
+				>
+					<img
+						className="rounded-full object-cover"
+						src={avatar}
+					/>
+				</div>
+			) : (
+				<div
+					className="text-primary-cl w-full h-full rounded-full flex justify-center items-center"
+					style={{ backgroundColor: color, fontSize: size }}
+				>
+					{name.charAt(0)}
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default ProfilePic;
