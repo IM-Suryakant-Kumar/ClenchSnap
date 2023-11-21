dc30d8120eeef75bb5a1fc63c490a1f18f61fe21
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-22 05:23:11 +0530

    feat: Added requireAuth func

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Sidebar from "./Sidebar";
 
 const Layout = () => {
+    
 	return (
 		<div className="min-h-screen">
 			<Navbar />
---
+++ client/src/utils/requireAuth.ts
@@ -0,0 +1,13 @@
+import { LoaderFunctionArgs, redirect } from "react-router-dom";
+import { getUserFromLocalStorage } from "./handleUser";
+
+const requireAuth = async ({ request }: LoaderFunctionArgs) => {
+	const pathname = new URL(request.url).pathname;
+	const user = getUserFromLocalStorage();
+	if (!user)
+		throw redirect(
+			`/login?message="You must login first&redirectTo=${pathname}`,
+		);
+};
+
+export default requireAuth;
