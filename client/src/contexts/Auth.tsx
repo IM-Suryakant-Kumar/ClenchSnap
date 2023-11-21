30ec785ba0009e61fbeea2122ce15c77890d1628
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-21 16:37:54 +0530

    refactor: auth context

--- client/src/contexts/Auth.tsx
+++ client/src/contexts/Auth.tsx
@@ -1,8 +1,6 @@
-import { createContext, useReducer, useContext, useMemo } from "react";
+import { createContext, useReducer, useContext } from "react";
 import { IAuthState } from "../types/statesAndActions";
 import { authInitialState, authReducer } from "../reducers/auth";
-import { ILogCred, IRegCred } from "../types/user";
-import { guestLogin, login, logout, signup } from "../utils/authApi";
 import IRes from "../types/response";
 import { getLoggedInUser } from "../utils/userApi";
 import { useLoading } from ".";
@@ -11,10 +9,6 @@ import loadingWrapper from "../utils/loadingWrapper";
 
 export interface IAuthContext {
 	authState: IAuthState;
-	registerUser: (cred: IRegCred) => Promise<void>;
-	loginUser: (cred: ILogCred) => Promise<void>;
-	loginGuestUser: () => Promise<void>;
-	logoutUser: () => Promise<void>;
 	getProfile: () => Promise<void>;
 }
 
@@ -26,58 +20,9 @@ type Props = {
 
 const AuthContextProvider: React.FC<Props> = ({ children }) => {
 	const [authState, authDispatch] = useReducer(authReducer, authInitialState);
-	const { loadingStart, loadingStop, submittingStart, submittingStop } =
-		useLoading() as ILoadingContext;
+	const { loadingStart, loadingStop } = useLoading() as ILoadingContext;
 
-	// register
-	const registerUser = async (cred: IRegCred) => {
-		const fn = async () => {
-			const { success, message } = (await signup(cred)) as IRes;
-			authDispatch({
-				type: "REGISTER",
-				payload: { errorMessage: success ? null : message },
-			});
-		};
-
-		loadingWrapper(submittingStart, submittingStop, fn);
-	};
-	//login
-	const loginUser = async (cred: ILogCred) => {
-		const fn = async () => {
-			const { success, message } = (await login(cred)) as IRes;
-			authDispatch({
-				type: "LOGIN",
-				payload: { errorMessage: success ? null : message },
-			});
-		};
-
-		loadingWrapper(submittingStart, submittingStop, fn);
-	};
-	// guest login
-	const loginGuestUser = async () => {
-		const fn = async () => {
-			const { success, message } = (await guestLogin()) as IRes;
-			authDispatch({
-				type: "GUEST_LOGIN",
-				payload: { errorMessage: success ? null : message },
-			});
-		};
-
-		loadingWrapper(submittingStart, submittingStop, fn);
-	};
-	// logout
-	const logoutUser = async () => {
-		const fn = async () => {
-			const { success, message } = (await logout()) as IRes;
-			authDispatch({
-				type: "LOGOUT",
-				payload: { errorMessage: success ? null : message },
-			});
-		};
-
-		loadingWrapper(loadingStart, loadingStop, fn);
-	};
-	// get logged-in user
+	// get profile
 	const getProfile = async () => {
 		const fn = async () => {
 			const { success, user } = (await getLoggedInUser()) as IRes;
@@ -90,14 +35,8 @@ const AuthContextProvider: React.FC<Props> = ({ children }) => {
 		loadingWrapper(loadingStart, loadingStop, fn);
 	};
 
-	useMemo(() => authState, []);
-
 	const providerItem = {
 		authState,
-		registerUser,
-		loginUser,
-		loginGuestUser,
-		logoutUser,
 		getProfile,
 	};
 
--- client/src/reducers/auth.ts
+++ client/src/reducers/auth.ts
@@ -7,22 +7,6 @@ export const authInitialState: IAuthState = {
 
 export const authReducer = (state: IAuthState, action: IAuthAction) => {
 	switch (action.type) {
-        case "REGISTER": {
-            const { errorMessage } = action.payload
-            return {...state, errorMessage}
-        }
-        case "LOGIN": {
-            const { errorMessage } = action.payload
-            return {...state, errorMessage}
-        }
-        case "GUEST_LOGIN": {
-            const { errorMessage } = action.payload
-            return {...state, errorMessage}
-        }
-        case "LOGOUT": {
-            const { errorMessage } = action.payload
-            return {...state, errorMessage}
-        }
         case "GET_PROFILE": {
             const { user } = action.payload
             return {...state, user}
