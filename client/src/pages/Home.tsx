2299c0d41584575f3f8e330818fcf33d44d06e7d
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-27 10:27:19 +0530

    fix: fix vercel route problem

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,7 @@
 import RightSidebar from "../components/RightSidebar";
 
 const Home = () => {
-	return <div>
-        <RightSidebar /></div>;
+	return <div><RightSidebar /></div>;
 };
 
 export default Home;
