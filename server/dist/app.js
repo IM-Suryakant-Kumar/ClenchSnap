792310b7c8909a88b8fba85c1be51fe0e33b1773
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-27 09:27:14 +0530

    build: updated build

--- server/dist/app.js
+++ server/dist/app.js
@@ -31,7 +31,7 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use((0, helmet_1.default)());
 app.use((0, morgan_1.default)("tiny"));
-app.use((0, cors_1.default)({ origin: CLIENT_URL, credentials: true }));
+app.use((0, cors_1.default)({ origin: [CLIENT_URL], credentials: true }));
 app.use((0, cookie_parser_1.default)());
 // routers
 app.use("/auth", auth_1.default);
