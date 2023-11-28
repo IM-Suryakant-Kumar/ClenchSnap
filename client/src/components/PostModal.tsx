5d1f26babd22af0e35df6f4ad97d30a005a65249
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-11-28 16:28:28 +0530

    feat: implemented emojipicker

--- client/src/components/PostModal.tsx
+++ client/src/components/PostModal.tsx
@@ -1,28 +1,96 @@
-import { useUser } from "../contexts";
-import ProfilePic from "./ProfilePic";
+import {
+	MdClose,
+	MdOutlineEmojiEmotions,
+	MdOutlineImage,
+} from "react-icons/md";
+import EmojiPicker, { EmojiClickData } from "emoji-picker-react";
+
+import { useState } from "react";
 
 type Props = {
-    toggleModal: boolean;
-}
+	toggleModal: boolean;
+	handleToggle: () => void;
+};
 
-const PostModal: React.FC<Props> = ({ toggleModal }) => {
-	const {
-		userState: { user },
-	} = useUser();
+const PostModal: React.FC<Props> = ({ toggleModal, handleToggle }) => {
+	const [toggleEmojiPicker, setToggleEmojiPicker] = useState<boolean>(true);
+
+    // toggle emoji
+	const handleEmojiPicker = () => {
+		setToggleEmojiPicker(prevState => !prevState);
+	};
+
+	const handleEmojiClick = (
+		emojiData: EmojiClickData,
+		event: MouseEvent,
+	) => {
+		console.log(emojiData.emoji);
+        handleEmojiPicker()
+	};
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+	};
 
 	return (
-		<form className={`${toggleModal ? "hidden" : "block" } md:w-[70%] bg-secondary-cl mt-[5em] mx-[0.8em] flex justify-center items-center rounded-md`}>
-			<div className="w-[15%] justify-self-end">
-				<ProfilePic
-					width="3rem"
-					height="3rem"
-					size="1rem"
-					name={`${user?.fullname}`}
-					avatar={`${user?.avatar}`}
-				/>
+		<div
+			className={`${
+				toggleModal ? "hidden" : "block"
+			} fixed left-0 top-0 right-0 bottom-0 z-50 bg-black bg-opacity-20 flex justify-center items-center`}>
+			<div
+				className={`w-[90%] max-w-[30rem] min-h-[13rem] pb-[1em] relative bg-secondary-cl
+            `}>
+				<div
+					className="absolute top-0 right-0 w-[1.5rem] h-[1.5rem] flex justify-center items-center text-lg bg-primary-cl text-red-500 hover:bg-red-500 hover:text-primary-cl cursor-pointer
+                "
+					onClick={handleToggle}>
+					<MdClose />
+				</div>
+				<form
+					className="flex flex-col justify-center items-center rounded-md mt-[2em]"
+					onSubmit={handleSubmit}>
+					<textarea
+						className="w-[90%] h-[4rem] outline-none  border-2  border-logo-cl rounded-md
+                    "
+						name="content"
+						placeholder="What is happening?!"></textarea>
+					{/* Action buttons */}
+					<div className="relative w-[90%] mt-[1em] flex">
+						<div className="w-[2rem] h-[2rem]">
+							<div className="absolute flex justify-center items-center text-[2rem] text-logo-cl cursor-pointer">
+								<MdOutlineImage />
+							</div>
+							<input
+								className="w-full h-full opacity-0"
+								type="file"
+								accept="image/png, image/jpg, image/jpeg"
+								name="image"
+							/>
+						</div>
+						<div
+							className="w-[2rem] h-[2rem] text-[2rem] text-logo-cl cursor-pointer ml-[0.3em]"
+							onClick={handleEmojiPicker}>
+							<MdOutlineEmojiEmotions />
+						</div>
+						<button className="ml-auto w-[4rem] h-[2rem] rounded-sm bg-logo-cl text-primary-cl cursor-pointer">
+							Post
+						</button>
+					</div>
+				</form>
+				<div
+					className={`${
+						toggleEmojiPicker ? "hidden" : "block"
+					} mt-[1em] flex justify-center`}>
+					<EmojiPicker
+						width="95%"
+						height="20em"
+						searchDisabled={true}
+						previewConfig={{ showPreview: false }}
+						onEmojiClick={handleEmojiClick}
+					/>
+				</div>
 			</div>
-			<textarea className="w-[82%] h-[2rem] resize-none outline-none border-2 border-logo-cl rounded-md"></textarea>
-		</form>
+		</div>
 	);
 };
 
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,13 +6,17 @@ import { useState } from "react";
 const Home = () => {
 	const [toggleModal, setToggleModal] = useState<boolean>(true);
 
+    const handleToggle = () => {
+        setToggleModal(prevState => !prevState)
+    }
+
 	return (
-		<div className="relative">
-			<PostModal toggleModal={toggleModal} />
+		<div className="relative min-h-screen">
+			<PostModal toggleModal={toggleModal} handleToggle={handleToggle} />
 			<RightSidebar />
 			<div
 				className="w-[3rem] h-[3rem] fixed bottom-[2.5em] sm:bottom-[0.5em] right-[0.5em] z-40  text-[1.5rem] p-[0.5em] text-primary-cl bg-logo-cl rounded-full cursor-pointer"
-				onClick={() => setToggleModal((prevState) => !prevState)}
+				onClick={handleToggle}
 			>
 				<IoPencil />
 			</div>
