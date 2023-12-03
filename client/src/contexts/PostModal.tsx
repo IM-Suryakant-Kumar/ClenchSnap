391338cef4bbfe4f3275f19b508f6778afa9ef5c
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-03 13:00:40 +0530

    feat: implemented handleModal func

--- client/src/contexts/PostModal.tsx
+++ client/src/contexts/PostModal.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState } from "react";
 
 interface IPosPostModalContext {
 	toggleModal: boolean;
-	setToggleModal: React.Dispatch<React.SetStateAction<boolean>>;
+	handleToggle: () => void;
 }
 
 const PostModalContext = createContext<IPosPostModalContext | null>(null);
@@ -14,13 +14,17 @@ type Props = {
 const PostModalContextProvider: React.FC<Props> = ({ children }) => {
 	const [toggleModal, setToggleModal] = useState<boolean>(true);
 
+    const handleToggle = () => {
+		setToggleModal(prevState => !prevState);
+	};
+
 	return (
-		<PostModalContext.Provider value={{ toggleModal, setToggleModal }}>
+		<PostModalContext.Provider value={{ toggleModal, handleToggle }}>
 			{children}
 		</PostModalContext.Provider>
 	);
 };
 
-const usePostModal = () => useContext(PostModalContext);
+const usePostModal = () => useContext(PostModalContext) as IPosPostModalContext;
 
 export { PostModalContextProvider, usePostModal };
