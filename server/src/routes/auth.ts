60d639faaa1d9f516b800de30fcc88c7efce32de
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-04 09:29:18 +0530

    fix: fix auth route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { createUser, guestLogin, login, logout } from "../controllers/auth";
-import { authenticateUser } from "../middleware";
 
 const router = Router();
 
 router.route("/register").post(createUser);
 router.route("/login").post(login);
 router.route("/guest-login").get(guestLogin);
-router.route("/logout").get(authenticateUser, logout);
+router.route("/logout").get(logout);
 
 export default router;
